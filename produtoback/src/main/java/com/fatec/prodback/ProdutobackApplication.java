716ea13fa3e70c778519c7a4282b9d1a83938527
ffvprogweb <disclabes@gmail.com>
2024-03-03 10:55:20 -0300

    Versao inicial

---
+++ produtoback/src/main/java/com/fatec/prodback/ProdutobackApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.prodback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutobackApplication.class, args);
+	}
+
+}
---
+++ produtoback/src/test/java/com/fatec/prodback/ProdutobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.prodback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
