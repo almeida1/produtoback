3db68f931b2aff6cda34ba076020b8cc8b2ace4a
ffvprogweb <disclabes@gmail.com>
2024-03-03 11:03:57 -0300

    Inclusao do comentario

--- produtoback/src/main/java/com/fatec/prodback/controller/ApiControle.java
+++ produtoback/src/main/java/com/fatec/prodback/controller/ApiControle.java
@@ -2,7 +2,9 @@ package com.fatec.prodback.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * trata as requisicoes http
+ */
 @RestController
 public class ApiControle {
 	
